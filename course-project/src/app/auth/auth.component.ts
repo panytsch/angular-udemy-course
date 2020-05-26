e7cc8e57c0f19701f598c135010d80d50e5b6973
Roman Panasiuk <gfyroman@gmail.com>
2020-05-26 15:11:59 +0300

    auth form

--- course-project/src/app/auth/auth.component.ts
+++ course-project/src/app/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -6,12 +7,25 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent implements OnInit {
-
+  isLoginMode = true;
+  form: FormGroup;
 
   constructor() {
   }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, Validators.minLength(6)),
+    });
   }
 
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
+
+  onSubmit(): void {
+    console.log(this.form.value);
+    this.form.reset();
+  }
 }
