ff58b5c2008df6f84553c8b3f1e37b87c655567f
Roman Panasiuk <gfyroman@gmail.com>
2020-05-27 15:17:03 +0300

    auth module

--- course-project/src/app/app.module.ts
+++ course-project/src/app/app.module.ts
@@ -3,32 +3,31 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {FormsModule} from '@angular/forms';
 import {AppRoutingModule} from './routing/app-routing.module';
-import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
-import {AuthComponent} from './auth/auth.component';
-import {AuthInterceptor} from './auth/auth.interceptor';
+import {HttpClientModule} from '@angular/common/http';
 import {RecipesModule} from './recipes/recipes.module';
 import {ShoppingListModule} from './shopping-list/shopping-list.module';
 import {SharedModule} from './shared/shared.module';
+import {CoreModule} from './core.module';
+import {AuthModule} from './auth/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AuthComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule,
     HttpClientModule,
     AppRoutingModule,
     RecipesModule,
     ShoppingListModule,
-    SharedModule
+    SharedModule,
+    CoreModule,
+    AuthModule
   ],
-  providers: [{provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}],
   bootstrap: [AppComponent]
 })
 export class AppModule {
---
+++ course-project/src/app/auth/auth.module.ts
@@ -0,0 +1,18 @@
+import {NgModule} from '@angular/core';
+import {AuthComponent} from './auth.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {SharedModule} from '../shared/shared.module';
+import {RouterModule} from '@angular/router';
+
+@NgModule({
+  declarations: [
+    AuthComponent,
+  ],
+  imports: [
+    ReactiveFormsModule,
+    RouterModule,
+    SharedModule
+  ]
+})
+export class AuthModule {
+}
